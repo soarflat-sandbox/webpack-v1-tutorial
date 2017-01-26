e5c8d8f5649f370676ed37139bdfd1068671c0b8
soarflat <soarflat777@gmail.com>
2017-01-27 01:36:21 +0900

    webpack.config.jsを修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 module.exports = {
+  // エントリーポイントの設定
   entry: './src/js/app.js',
+  // 出力の設定
   output: {
+    // 出力するファイル名
     filename: 'bundle.js',
+    // 出力先のパス
     path: './public/js'
   },
+  // ローダーの設定
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
+    loaders: [{
+      // ローダーの処理対象ファイル
+      test: /\.js$/,
+      // ローダーの処理対象から外すディレクトリ
+      exclude: /node_modules/,
+      // 利用するローダー
+      loader: 'babel-loader?presets[]=es2015'
+    }]
   }
 };
